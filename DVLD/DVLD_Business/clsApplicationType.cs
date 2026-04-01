99315b0b521bee03c7d11810f157b334f1448131
OMAR-ALASHLEM <omarwesamdeveloper@gmail.com>
2026-04-01 16:14:16 +0200

    feat: implement clsApplicationType(BLL) with Find, Update, and Save functionality

---
+++ DVLD/DVLD_Business/clsApplicationType.cs
@@ -0,0 +1,73 @@
+﻿using DVLD_DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Business
+{
+    public class clsApplicationType
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+
+        public int ID { set; get; }
+        public string Title { set; get; }
+        public float Fees { set; get; }
+
+        public clsApplicationType()
+
+        {
+            this.ID = -1;
+            this.Title = "";
+            this.Fees = 0;
+            Mode = enMode.AddNew;
+
+        }
+
+        public clsApplicationType(int ID, string ApplicationTypeTitel, float ApplicationTypeFees)
+
+        {
+            this.ID = ID;
+            this.Title = ApplicationTypeTitel;
+            this.Fees = ApplicationTypeFees;
+            Mode = enMode.Update;
+        }
+
+        public DataTable GetAllApplicationTypes()
+        {
+            return clsApplicationTypesData.GetAllApplicationTypes();
+
+        }
+        private bool _Update()
+        {
+            return clsApplicationTypesData.UpdateApplicationType(this.ID, this.Title, this.Fees);
+        }
+
+        public bool Save()
+        {
+
+            if (Mode==enMode.Update)
+            {
+                return  _Update();
+            }
+            else
+            {
+                return false; 
+            }
+        }
+
+        public clsApplicationType GetApplicationTypeByID(int ApplicationTypeID)
+        {
+            string Title="";float Fees=0;
+            if (clsApplicationTypesData.GetApplicationTypeInfoByID(ApplicationTypeID, ref Title, ref Fees))
+            {
+               return new clsApplicationType(ApplicationTypeID, Title, Fees);
+            }
+            return null;
+        }
+    }
+}
