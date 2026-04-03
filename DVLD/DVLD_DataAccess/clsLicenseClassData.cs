28faf5f701e5f13ef0ab3aadde18ddb134c930a7
OMAR-ALASHLEM <omarwesamdeveloper@gmail.com>
2026-04-03 20:52:58 +0200

    feat: implement DAL for License Classes (GetByID and GetAll)

---
+++ DVLD/DVLD_DataAccess/clsLicenseClassData.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccess
+{
+    public class clsLicenseClassData
+    {
+
+        public static bool GetLicenseClassInfoByID(int LicenseClassID,ref string ClassName, ref string ClassDescription, 
+            ref byte MinimumAllowedAge, ref byte DefaultValidityLength, ref float ClassFees) 
+        {
+            bool IsFound = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = "SELECT * FROM LicenseClasses WHERE LicenseClassID = @LicenseClassID";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@LicenseClassID", LicenseClassID);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    IsFound = true;
+                    ClassName = (string)reader["ClassName"];
+                    ClassDescription = (string)reader["ClassDescription"];
+                    MinimumAllowedAge = (byte)reader["MinimumAllowedAge"];
+                    DefaultValidityLength = (byte)reader["DefaultValidityLength"];
+                    ClassFees = Convert .ToSingle(reader["ClassFees"]);
+
+                }
+                reader.Close();
+            }
+            catch
+            { IsFound = false; }
+            finally
+            {
+            connection.Close();
+            }
+            return IsFound;
+        }
+        public static DataTable GetAllLicensesClasses()
+        {
+            DataTable table = new DataTable();
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = "SELECT * FROM LicenseClasses ";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    table.Load(reader);
+
+                }
+                reader.Close() ;
+            }
+            catch
+            {  }
+            finally
+            {
+                connection.Close();
+            }
+
+            return table;
+        }
+
+       
+
+    }
+}
