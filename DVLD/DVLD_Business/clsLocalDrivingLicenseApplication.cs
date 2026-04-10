7aacd4f036ee9f0467b20ff29cd5daf6c0c2e8ec
OMAR ALASHLEM <owesam113@gmail.com>
2026-04-10 16:06:45 +0200

    feat(bll): implement clsLocalDrivingLicenseApplication class
    Integrated inheritance from clsApplication.
    Added Save() logic for AddNew and Update modes.
    Implemented static methods: FindByLocalDrivingAppLicenseID and FindByApplicationID.
    Added Delete() and GetAllLocalDrivingLicenseApplications() operations.

---
+++ DVLD/DVLD_Business/clsLocalDrivingLicenseApplication.cs
@@ -0,0 +1,145 @@
+﻿using DVLD_DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Business
+{
+    public class clsLocalDrivingLicenseApplication : clsApplication
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int LocalDrivingLicenseApplicationID { get; set; }
+        public int LicenseClassID { get; set; }
+
+        public clsLicenseClass LicenseClassInfo;
+        public string PersonFullName => ApplicantFullName;
+        private clsLocalDrivingLicenseApplication(int LocalDrivingLicenseApplicationID, int ApplicationID, int ApplicantPersonID,
+             DateTime ApplicationDate, int ApplicationTypeID,
+              enApplicationStatus ApplicationStatus, DateTime LastStatusDate,
+              float PaidFees, int CreatedByUserID, int LicenseClassID)
+
+        {
+            this.LocalDrivingLicenseApplicationID = LocalDrivingLicenseApplicationID; ;
+            this.ApplicationID = ApplicationID;
+            this.ApplicantPersonID = ApplicantPersonID;
+            this.ApplicationDate = ApplicationDate;
+            this.ApplicationTypeID = ApplicationTypeID;
+            this.ApplicationStatus = ApplicationStatus;
+            this.LastStatusDate = LastStatusDate;
+            this.PaidFees = PaidFees;
+            this.CreatedByUserID = CreatedByUserID;
+            this.LicenseClassID = LicenseClassID;
+            this.LicenseClassInfo = clsLicenseClass.Find(LicenseClassID);
+            Mode = enMode.Update;
+        }
+        public clsLocalDrivingLicenseApplication()
+        {
+            this.LocalDrivingLicenseApplicationID = -1;
+            this.LicenseClassID = -1;
+            Mode = enMode.AddNew;
+        }
+
+        private bool _AddNewLocalDrivingLicenseApplication()
+        {
+            this.LocalDrivingLicenseApplicationID = clsLocalDrivingLicenseApplicationData.AddNewLocalDrivingLicenseApplication(this.ApplicationID, this.LicenseClassID);
+            return (this.LocalDrivingLicenseApplicationID != -1);
+        }
+        private bool _UpdateLocalDrivingLicenseApplication() => clsLocalDrivingLicenseApplicationData.UpdateLocalDrivingLicenseApplication(this.LocalDrivingLicenseApplicationID, this.ApplicationID, this.LicenseClassID);
+
+        public static clsLocalDrivingLicenseApplication FindByLocalDrivingAppLicenseID(int LocalDrivingLicenseApplicationID)
+        {
+            int ApplicationID = -1, LicenseClassID = -1;
+
+
+            if (clsLocalDrivingLicenseApplicationData.GetLocalDrivingLicenseApplicationInfoByID(LocalDrivingLicenseApplicationID, ref ApplicationID, ref LicenseClassID))
+            {
+                clsApplication Application = clsApplication.Find(ApplicationID);
+
+                return new clsLocalDrivingLicenseApplication(LocalDrivingLicenseApplicationID, Application.ApplicationID, Application.ApplicantPersonID,
+                 Application.ApplicationDate, Application.ApplicationTypeID, (enApplicationStatus)Application.ApplicationStatus, Application.LastStatusDate,
+                 Application.PaidFees, Application.CreatedByUserID, LicenseClassID);
+
+            }
+            else
+                return null;
+
+        }
+
+        public static clsLocalDrivingLicenseApplication FindByApplicationID(int ApplicationID)
+        {
+            // 
+            int LocalDrivingLicenseApplicationID = -1, LicenseClassID = -1;
+
+            if (clsLocalDrivingLicenseApplicationData.GetLocalDrivingLicenseApplicationInfoByApplicationID(ApplicationID, ref LocalDrivingLicenseApplicationID, ref LicenseClassID))
+            {
+                clsApplication Application = clsApplication.Find(ApplicationID);
+
+
+                return new clsLocalDrivingLicenseApplication(
+                    LocalDrivingLicenseApplicationID, Application.ApplicationID,
+                    Application.ApplicantPersonID, Application.ApplicationDate, Application.ApplicationTypeID,
+                     (enApplicationStatus)Application.ApplicationStatus, Application.LastStatusDate,
+                      Application.PaidFees, Application.CreatedByUserID, LicenseClassID);
+
+            }
+            else
+                return null;
+
+
+        }
+
+        public bool Save()
+        {
+
+         
+            base.Mode = (clsApplication.enMode)Mode;
+            if (!base.Save())
+                return false;
+
+
+      
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewLocalDrivingLicenseApplication())
+                    {
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else  return false;
+                  
+
+                case enMode.Update:
+
+                    return _UpdateLocalDrivingLicenseApplication();
+
+            }
+
+            return false;
+        }
+
+        public static DataTable GetAllLocalDrivingLicenseApplications() => clsLocalDrivingLicenseApplicationData.GetAllLocalDrivingLicenseApplications();
+
+        public bool Delete()
+        {
+            bool IsLocalDrivingApplicationDeleted = false;
+            bool IsBaseApplicationDeleted = false;
+    
+            IsLocalDrivingApplicationDeleted = clsLocalDrivingLicenseApplicationData.DeleteLocalDrivingLicenseApplication(this.LocalDrivingLicenseApplicationID);
+
+            if (!IsLocalDrivingApplicationDeleted)
+                return false;
+
+
+            IsBaseApplicationDeleted = base.Delete();
+            return IsBaseApplicationDeleted;
+
+        }
+
+    }
+}
