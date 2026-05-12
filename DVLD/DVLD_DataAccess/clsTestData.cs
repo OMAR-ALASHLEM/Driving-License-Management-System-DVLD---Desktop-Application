d5fd20fd9ed0a7e079c6fb13109b01e599b6a0e0
OMAR ALASHLEM <owesam113@gmail.com>
2026-05-12 20:03:07 +0200

    Add method to fetch last test by person, type, and class
    Added GetLastTestByPersonAndTestTypeAndLicenseClass to clsTestData.cs. This method retrieves the latest test record for a specific person, license class, and test type, returning relevant details via reference parameters. Handles SQL query execution, parameter binding, and error management.

--- DVLD/DVLD_DataAccess/clsTestData.cs
+++ DVLD/DVLD_DataAccess/clsTestData.cs
@@ -254,6 +254,78 @@ namespace DVLD_DataAccess
 
         }
 
+        public static bool GetLastTestByPersonAndTestTypeAndLicenseClass (int PersonID, int LicenseClassID, int TestTypeID, ref int TestID,
+             ref int TestAppointmentID, ref bool TestResult,
+             ref string Notes, ref int CreatedByUserID)
+        {
+            bool isFound = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"SELECT  top 1 Tests.TestID, 
+                Tests.TestAppointmentID, Tests.TestResult, 
+			    Tests.Notes, Tests.CreatedByUserID, Applications.ApplicantPersonID
+                FROM            LocalDrivingLicenseApplications INNER JOIN
+                                         Tests INNER JOIN
+                                         TestAppointments ON Tests.TestAppointmentID = TestAppointments.TestAppointmentID ON LocalDrivingLicenseApplications.LocalDrivingLicenseApplicationID = TestAppointments.LocalDrivingLicenseApplicationID INNER JOIN
+                                         Applications ON LocalDrivingLicenseApplications.ApplicationID = Applications.ApplicationID
+                WHERE        (Applications.ApplicantPersonID = @PersonID) 
+                        AND (LocalDrivingLicenseApplications.LicenseClassID = @LicenseClassID)
+                        AND ( TestAppointments.TestTypeID=@TestTypeID)
+                ORDER BY Tests.TestAppointmentID DESC";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@PersonID", PersonID);
+            command.Parameters.AddWithValue("@LicenseClassID", LicenseClassID);
+            command.Parameters.AddWithValue("@TestTypeID", TestTypeID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+
+                
+                    isFound = true;
+                    TestID = (int)reader["TestID"];
+                    TestAppointmentID = (int)reader["TestAppointmentID"];
+                    TestResult = (bool)reader["TestResult"];
+                    if (reader["Notes"] == DBNull.Value)
+
+                        Notes = "";
+                    else
+                        Notes = (string)reader["Notes"];
+
+                    CreatedByUserID = (int)reader["CreatedByUserID"];
+
+                }
+                else
+                {
+                   
+                    isFound = false;
+                }
+
+                reader.Close();
+
+
+            }
+            catch 
+            {
+               
+                isFound = false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isFound;
+        }
+          
+
 
 
 
