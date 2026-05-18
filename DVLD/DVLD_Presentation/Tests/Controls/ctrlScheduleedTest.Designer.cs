63558c2d10fbd80ba552c2656d05b25d2429d45d
OMAR ALASHLEM <owesam113@gmail.com>
2026-05-18 23:23:59 +0200

    Add ctrlScheduleedTest user control for test scheduling
    Introduced a new user control `ctrlScheduleedTest` to manage and display test scheduling information. Updated `DVLD_Presentation.csproj` to include the new control and its associated files. Implemented properties and methods to handle test data and designed a detailed UI layout. Added resource entries to support the control's functionality and appearance.

---
+++ DVLD/DVLD_Presentation/Tests/Controls/ctrlScheduleedTest.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace DVLD_Presentation.Tests.Controls
+{
+    partial class ctrlScheduleedTest
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ctrlScheduleedTest));
+            this.gbTestType = new System.Windows.Forms.GroupBox();
+            this.lblTestID = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblDrivingClass = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblLocalDrivingLicenseAppID = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblFullName = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblTrial = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblFees = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pbTestTypeImage = new System.Windows.Forms.PictureBox();
+            this.gbTestType.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTestTypeImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbTestType
+            // 
+            this.gbTestType.Controls.Add(this.lblTestID);
+            this.gbTestType.Controls.Add(this.label4);
+            this.gbTestType.Controls.Add(this.lblDate);
+            this.gbTestType.Controls.Add(this.lblDrivingClass);
+            this.gbTestType.Controls.Add(this.label10);
+            this.gbTestType.Controls.Add(this.lblLocalDrivingLicenseAppID);
+            this.gbTestType.Controls.Add(this.label8);
+            this.gbTestType.Controls.Add(this.lblFullName);
+            this.gbTestType.Controls.Add(this.label6);
+            this.gbTestType.Controls.Add(this.lblTrial);
+            this.gbTestType.Controls.Add(this.label5);
+            this.gbTestType.Controls.Add(this.lblFees);
+            this.gbTestType.Controls.Add(this.label2);
+            this.gbTestType.Controls.Add(this.label1);
+            this.gbTestType.Controls.Add(this.lblTitle);
+            this.gbTestType.Controls.Add(this.pbTestTypeImage);
+            this.gbTestType.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold);
+            this.gbTestType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(37)))), ((int)(((byte)(41)))));
+            this.gbTestType.Location = new System.Drawing.Point(3, 8);
+            this.gbTestType.Name = "gbTestType";
+            this.gbTestType.Size = new System.Drawing.Size(433, 698);
+            this.gbTestType.TabIndex = 228;
+            this.gbTestType.TabStop = false;
+            this.gbTestType.Text = "Test Type ";
+            // 
+            // lblTestID
+            // 
+            this.lblTestID.AutoSize = true;
+            this.lblTestID.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblTestID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(37)))), ((int)(((byte)(41)))));
+            this.lblTestID.Location = new System.Drawing.Point(253, 625);
+            this.lblTestID.Name = "lblTestID";
+            this.lblTestID.Size = new System.Drawing.Size(107, 20);
+            this.lblTestID.TabIndex = 243;
+            this.lblTestID.Text = "Not Taken Yet";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(58)))), ((int)(((byte)(64)))));
+            this.label4.Image = global::DVLD_Presentation.Properties.Resources.Number_32;
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label4.Location = new System.Drawing.Point(13, 615);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 40);
+            this.label4.TabIndex = 242;
+            this.label4.Text = "        Test ID:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblDate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(37)))), ((int)(((byte)(41)))));
+            this.lblDate.Location = new System.Drawing.Point(253, 513);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(113, 20);
+            this.lblDate.TabIndex = 241;
+            this.lblDate.Text = "[dd/mm/yyyy]";
+            // 
+            // lblDrivingClass
+            // 
+            this.lblDrivingClass.AutoSize = true;
+            this.lblDrivingClass.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblDrivingClass.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(37)))), ((int)(((byte)(41)))));
+            this.lblDrivingClass.Location = new System.Drawing.Point(253, 329);
+            this.lblDrivingClass.Name = "lblDrivingClass";
+            this.lblDrivingClass.Size = new System.Drawing.Size(70, 20);
+            this.lblDrivingClass.TabIndex = 240;
+            this.lblDrivingClass.Text = "[???????]";
+            // 
+            // label10
+            // 
+            this.label10.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold);
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(58)))), ((int)(((byte)(64)))));
+            this.label10.Image = global::DVLD_Presentation.Properties.Resources.License_Type_32;
+            this.label10.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label10.Location = new System.Drawing.Point(10, 323);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(164, 40);
+            this.label10.TabIndex = 239;
+            this.label10.Text = "        Driving Class:";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblLocalDrivingLicenseAppID
+            // 
+            this.lblLocalDrivingLicenseAppID.AutoSize = true;
+            this.lblLocalDrivingLicenseAppID.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblLocalDrivingLicenseAppID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(123)))), ((int)(((byte)(255)))));
+            this.lblLocalDrivingLicenseAppID.Location = new System.Drawing.Point(253, 269);
+            this.lblLocalDrivingLicenseAppID.Name = "lblLocalDrivingLicenseAppID";
+            this.lblLocalDrivingLicenseAppID.Size = new System.Drawing.Size(35, 20);
+            this.lblLocalDrivingLicenseAppID.TabIndex = 238;
+            this.lblLocalDrivingLicenseAppID.Text = "[??]";
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold);
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(58)))), ((int)(((byte)(64)))));
+            this.label8.Image = global::DVLD_Presentation.Properties.Resources.Number_32;
+            this.label8.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label8.Location = new System.Drawing.Point(13, 263);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(132, 40);
+            this.label8.TabIndex = 237;
+            this.label8.Text = "        D.L. App ID:";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblFullName
+            // 
+            this.lblFullName.AutoSize = true;
+            this.lblFullName.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblFullName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(37)))), ((int)(((byte)(41)))));
+            this.lblFullName.Location = new System.Drawing.Point(253, 389);
+            this.lblFullName.Name = "lblFullName";
+            this.lblFullName.Size = new System.Drawing.Size(70, 20);
+            this.lblFullName.TabIndex = 236;
+            this.lblFullName.Text = "[???????]";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold);
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(58)))), ((int)(((byte)(64)))));
+            this.label6.Image = global::DVLD_Presentation.Properties.Resources.Person_32;
+            this.label6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label6.Location = new System.Drawing.Point(13, 383);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(132, 40);
+            this.label6.TabIndex = 235;
+            this.label6.Text = "        Full Name:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblTrial
+            // 
+            this.lblTrial.AutoSize = true;
+            this.lblTrial.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblTrial.ForeColor = System.Drawing.Color.Crimson;
+            this.lblTrial.Location = new System.Drawing.Point(253, 449);
+            this.lblTrial.Name = "lblTrial";
+            this.lblTrial.Size = new System.Drawing.Size(35, 20);
+            this.lblTrial.TabIndex = 234;
+            this.lblTrial.Text = "[??]";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(58)))), ((int)(((byte)(64)))));
+            this.label5.Image = global::DVLD_Presentation.Properties.Resources.Count_32;
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label5.Location = new System.Drawing.Point(13, 443);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 40);
+            this.label5.TabIndex = 233;
+            this.label5.Text = "        Trial:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblFees
+            // 
+            this.lblFees.AutoSize = true;
+            this.lblFees.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.lblFees.ForeColor = System.Drawing.Color.ForestGreen;
+            this.lblFees.Location = new System.Drawing.Point(253, 569);
+            this.lblFees.Name = "lblFees";
+            this.lblFees.Size = new System.Drawing.Size(49, 21);
+            this.lblFees.TabIndex = 232;
+            this.lblFees.Text = "[$$$]";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(58)))), ((int)(((byte)(64)))));
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Location = new System.Drawing.Point(13, 563);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 40);
+            this.label2.TabIndex = 231;
+            this.label2.Text = "        Fees:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(58)))), ((int)(((byte)(64)))));
+            this.label1.Image = global::DVLD_Presentation.Properties.Resources.Calendar_32;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Location = new System.Drawing.Point(13, 503);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 40);
+            this.label1.TabIndex = 230;
+            this.label1.Text = "        Test Date:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 26F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(37)))), ((int)(((byte)(41)))));
+            this.lblTitle.Location = new System.Drawing.Point(21, 176);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(332, 60);
+            this.lblTitle.TabIndex = 229;
+            this.lblTitle.Text = "Schedule Test";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pbTestTypeImage
+            // 
+            this.pbTestTypeImage.Image = global::DVLD_Presentation.Properties.Resources.Vision_512;
+            this.pbTestTypeImage.Location = new System.Drawing.Point(148, 19);
+            this.pbTestTypeImage.Name = "pbTestTypeImage";
+            this.pbTestTypeImage.Size = new System.Drawing.Size(112, 140);
+            this.pbTestTypeImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbTestTypeImage.TabIndex = 228;
+            this.pbTestTypeImage.TabStop = false;
+            // 
+            // ctrlScheduleedTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gbTestType);
+            this.Name = "ctrlScheduleedTest";
+            this.Size = new System.Drawing.Size(439, 709);
+            this.Load += new System.EventHandler(this.ctrlScheduleedTest_Load);
+            this.gbTestType.ResumeLayout(false);
+            this.gbTestType.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTestTypeImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbTestType;
+        private System.Windows.Forms.Label lblTestID;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblDrivingClass;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblLocalDrivingLicenseAppID;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblFullName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblTrial;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblFees;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.PictureBox pbTestTypeImage;
+    }
+}
